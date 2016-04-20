dee4af3caf7121f6db93888d7b2829b2e5abadf6
zhuoyikang <zhuoyikang@gmail.com>
2016-04-20 12:21:45 +0800

    mnesia test

--- mt.erl
+++ mt.erl
@@ -9,12 +9,17 @@ create_disk() ->
     mnesia:create_table(test, [{disc_copies, [node()]}, {attributes, [a,b,c,e]}]).
 
 
-write_over_2g() ->
+write_over_2g(0,_) ->
+    ok;
+write_over_2g(N,Item) ->
+    mnesia:sync_transaction(fun() ->
+                                    mnesia:write({test,N,Item,a,a})
+                            end),
+    write_over_2g(N-1).
+
+write_over_2g(N) ->
     Item = lists:duplicate(10000,"abc"),
-    [mnesia:sync_transaction(fun() ->
-                                     mnesia:write({test,A,Item,a,a})
-                             end)  || A <- lists:seq(1,100000)],
-    ok.
+    write_over_2g(N,Item).
 
 test_insert() ->
     [mnesia:dirty_write(test, {test,Key,a,a,a})||Key  <- lists:seq(1,200000)].
@@ -27,3 +32,10 @@ report() ->
     {Time2, _} = timer:tc(?MODULE, test_read,[]),
     io:format("insert ~p\n read ~p\n", [Time2,Time1]),
     ok.
+
+%% 尝试写入超过2G.
+dets() ->
+    {ok, F} = dets:open_file(test,[{type,set},{auto_save,10}]),
+    Item = lists:duplicate(10000,"abc"),
+    [dets:insert(F,{{seq,A},Item}) || A <- lists:seq(1,100000)],
+    ol.
